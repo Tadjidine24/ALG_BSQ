72ca740e58f4e05d33bb37825f7920d7e09b7fba
Tamou Tadjidine <tadjidine.tamou@epitech.eu>
2020-07-16 17:10:51 +0200

    ...

--- test.php
+++ test.php
@@ -1,31 +1,30 @@
-<?php
-#! /usr/bin/perl −w
-if((scalar @ ARGV)!=3)
+# ! /usr/bin/perl −w
+if((scalar @ARGV)!=3)
 {
-    print"program x y density\n ";
-    exit;
+print"program x y density \n" ;
+exit ;
 }
-$x = $ARGV [0] ;
-$y = $ARGV [1] ;
-$density=$ARGV[2];
-$i = 0;
-$j = 0;
-print $y."\n ";
-while($i<$y)
+my $x = $ARGV[0] ;
+my $y = $ARGV[1] ;
+my $density = $ARGV[2] ;
+my $ i = 0;
+my $ j = 0;
+print $y . " \n" ;
+while ( $i < $y )
 {
-    $j = 0;
-    while($j<$x)
-    {
-        if (intval(rand($y)*2)<$density)
-        {
-            print" o " ;
-        }
-        else
-        {
-            print" . " ;
-        }
-        $j++;
-    }
-    print" \n " ;
-    $i++ ;
+$j = 0;
+while ($j < $x )
+{
+if(int(rand($y)*2) < $density)
+{
+print "o" ;
+}
+else
+{
+print "." ;
+}
+$j++ ;
+}
+print "\n" ;
+$i++ ;
 }
